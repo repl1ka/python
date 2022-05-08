1d3c4efb84e0c9bb6d79c9fe8b67322d393d77e2
repl1ka <77627072+repl1ka@users.noreply.github.com>
2022-05-08 15:05:55 +0300

    Create 01.08.py

---
+++ 01/01.08.py
@@ -0,0 +1,7 @@
+first_name = input('Введите имя пользователя: ')
+greeting = 'Утро доброе'
+print(greeting +', '+ first_name)
+info = "К сожалению, у Вас нет доступа к системе."
+intro = "Пожалуйста, обратитесь к системному администратору."
+print(info)
+print(intro)
